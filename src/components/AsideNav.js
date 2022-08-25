d233ba31f6f6d40ac58c7a0ea9fd52ac83b1dab4
Bbromw <kaystim6@gmail.com>
2022-08-25 14:56:32 +0700

    do [p-5] [p-6]

---
+++ src/components/AsideNav.js
@@ -0,0 +1,45 @@
+import React, { useRef } from "react"
+import { Link } from "react-router-dom"
+import authorImg from "../assets/music.jpg"
+
+const AsideNav = () => {
+
+  const responsiveNav = useRef(null)
+  const handleToggle = () => {
+    responsiveNav.current.classList.toggle("active")
+  }
+  return (
+    <aside className="responsive-nav" ref={responsiveNav}>
+      <div className="toggle-menu" onClick={handleToggle}></div>
+      <div className="image">
+        <img className="author-img" src={authorImg} alt="" />
+      </div>
+      <nav className="app-sidenav">
+        <ul className="app-sidenav-list">
+          <li className="app-sidenav-list-item">
+            <Link className="app-sidenav__link" to="/">
+              Home
+            </Link>
+          </li>
+          <li className="app-sidenav-list-item">
+            <Link className="app-sidenav__link" to="/about">
+              About
+            </Link>
+          </li>
+          <li className="app-sidenav-list-item">
+            <Link className="app-sidenav__link" to="/portfolio">
+              Portfolio
+            </Link>
+          </li>
+          <li className="app-sidenav-list-item">
+            <Link className="app-sidenav__link" to="/contact">
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </aside>
+  )
+}
+
+export default AsideNav
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,44 @@
 import React from "react"
 
 const Home = () => {
+
+  useEffect(() => {
+    const list = document.querySelector(".list")
+    const text = document.querySelector(".lens-text")
+    const textList = ["Student", "GenZ", "Programing"]
+    let index = 0
+    const timer = setInterval(() => {
+      if (index === 3) {
+        index = 0
+      }
+      list.innerHTML = textList[index]
+      text.innerHTML = textList[index]
+      index++
+    }, 4000)
+    return () => {
+      clearInterval(timer)
+    }
+  }, [])
+
   return (
     <main className="main-content home">
       <div>
-        <h1 className="home__heading">TEGAN KAY</h1>
+        <h1 className="home__heading">Khoi Nguyen</h1>
       </div>
       <section className="creative-section">
         <h2>Creative</h2>
+
         <ul className="creative-section__list">
           <li className="creative-section__list-item">Designer</li>
           <li className="creative-section__list-item">Freelancer</li>
           <li className="creative-section__list-item">Developer</li>
         </ul>
+        <div className="list-container">
+          <p className="list">GenZ</p>
+          <div className="list-lens">
+            <p className="lens-text">GenZ</p>
+          </div>
+        </div>
       </section>
       <div>
         <button className="button home-btn">Get in Touch</button>
