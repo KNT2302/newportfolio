3f9aa6329edebfae248bf54c91243776a55e1a75
Bbromw <kaystim6@gmail.com>
2022-08-25 10:40:24 +0700

    hompage and aside-navigation

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 export { default as AppHeader } from "./AppHeader"
 export { default as Author } from "./Author"
 export { default as Footer } from "./Footer"
+export { default as AsideNav } from "./AsideNav"
 
--- src/pages/About.js
+++ src/pages/About.js
@@ -2,7 +2,20 @@ import React from 'react'
 
 const About = () => {
   return (
-    <main className='main-content'>About</main>
+    <main className='main-content about-page'>
+      <article className="introduce">
+        <h2 className='introduce__heading'>
+          ABOUT ME
+        </h2>
+        <section className='introduce__content'>
+          <div className='speech'>
+            Hello there! My name is Alan Walker. I am a graphic designer, and I'm very passionate and dedicated to my work.
+
+            With 20 years experience as a professional a graphic designer, I have acquired the skills and knowledge necessary to make your project a success.
+          </div>
+        </section>
+      </article>
+    </main>
   )
 }
 
